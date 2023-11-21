ed7647217da5db838c32be1c54c2c86deaca1c1a
Brennen N <brennennalley@gmail.com>
2023-11-20 20:40:37 -0500

    Update Program.cs

--- Arrays and ForEach/Program.cs
+++ Arrays and ForEach/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//declare string array
+string[] fraudulentOrderIds = new string[3];
+fraudulentOrderIds[0]= "A123";
+fraudulentOrderIds[1]= "B456";
+fraudulentOrderIds[2]= "C789";
